120cd899d72db8166d8d489a6da44f93ddbce033
sampss <104110725+im-sampss@users.noreply.github.com>
2024-12-10 12:16:06 +0800

    Write-Your-Name.c

--- Write-Your-Name.c
+++ Write-Your-Name.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
 	
 	char name[50];
 	
 	printf("Please Enter Your Name: ");
-	scanf("%s", name);
+	fgets(name, sizeof(name), stdin);
+	
+	size_t length = strlen(name);
+	if (length > 0 && name[length - 1] == '\n') name[length - 1] = '\0';
 	
 	printf("Your name is: %s", name);
+	printf("\nLength of input: %zu", strlen(name));
 	return 0;
 }
